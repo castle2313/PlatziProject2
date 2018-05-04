e50bc4dd6669635216da381a305bc3800a349b41
Razo Mata <castle@macbook-pro-de-macbook.local>
2018-05-03 20:32:50 -0500

    Initial Commit

---
+++ PlatziProject/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  PlatziProject
+//
+//  Created by Razo Mata on 03/05/18.
+//  Copyright © 2018 Razo Mata. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
